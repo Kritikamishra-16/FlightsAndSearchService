ca7fbb3778cfadab6822f21a87baa971117572da
Kritika Mishra <mishrakritika2001@gmail.com>
2022-12-24 16:01:38 +0530

    completed city-repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,26 +1,55 @@
 const { City }= require('../models/index');
 
 class CityRepository {
-    async createCity({ name }){
+
+    async createCity({name}){   //destructuring the object to use it directly
         try{
-            const city=await City.create({name});
+            //async call as we are communicating outside the JS server with db server so this functionaluty is not native to js thats why it is handled by runtime promise
+            const city= await City.create({ //pass the object
+                name  //name(model_property): name(object key)
+            }) 
             return city;
-        }catch(error){
+        }catch (error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 
-    async deleteCity(CityId){
+    async deleteCity(cityId){
         try{
             await City.destroy({
                 where:{
-                    id : CityId
+                    id: cityId
                 }
             });
+            return true;
         }catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+    async updateCity(cityId,data){
+        try{
+            const city=await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            });
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+    async getCity(cityId){
+        try{
+            const city=await City.findByPk(cityId);
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
 }
 
-module.exports= CityRepository;
+module.exports = CityRepository;
